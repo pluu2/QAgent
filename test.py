2f961c9392990e60d37aa52cb9a36b1cb14d33c4
Lulu Gao <lulugao@Lulus-MacBook-Air.local>
2021-01-12 15:08:50 -0500

    QAgent.py

--- test.py
+++ test.py
@@ -7,6 +7,19 @@ from utils.Utils import generate_square as GS
 from utils.Utils import dynamic_QM
 
 
-agent=Q_agent_base()
+agent=Q_agent_base(2)
 
-print(agent.states)
+agent.add_state('0212') #the states will have to be stored as a text
+agent.add_state('1232')
+
+print(agent.allstates())
+
+print(agent.search('0212'))
+print(agent.search(12312))
+
+print(agent.reward('0212',1,10))
+
+print(agent.table())
+
+print(agent.return_highest('0212'))
+print(agent.return_highest('1232'))
--- utils/QAgent.py
+++ utils/QAgent.py
@@ -1,17 +1,21 @@
 import numpy as np
 from utils.Utils import dynamic_QM
 class Q_agent_base(): 
-  def __init__(self): 
+  def __init__(self,starting_actions=2): #starting action is how many actions are initially present. 
     self.states = [] #will store the names of the states 
-    self.states.append(0) #first place holder
+    self.states.append('0')
     self.QM=dynamic_QM()
+    for _ in range(starting_actions):
+      self.QM.add_action()
     
   def add_state (self,state):  #add new state not before checking if it exists. 
     if  not(state in self.states):  #state does not exist
       self.states.append(state)
       self.QM.add_state()
       
-      
+  def total_actions(self): 
+    return self.QM.cols-1
+
   def add_action(self): 
       self.QM.add_action() #no need to check, this action literally adds in numerical order
   
@@ -21,8 +25,20 @@ class Q_agent_base():
   def allstates(self):
     return self.states
 
+  def search (self,search_value): #search and return index
+    try:
+      return self.states.index(search_value)
+    except: 
+      return -1 #if the state does not exist.
+
   def reward(self,state,action,reward): 
-    #try: #to ensure that only states that exist and actions that exist are used. 
-    self.QM.table[self.QM.search_states(state)[0]][action+1]=reward  #assumes the first value is the correct one, as there should be no duplciates
-    #except: 
-      # pass
+    try: #to ensure that only states that exist and actions that exist are used. 
+  
+     self.QM.table[self.states.index(state)][action+1]=reward
+    except:
+      pass
+    #self.QM.table[self.QM.search_states(state)[0]][action+1]=reward  #assumes the first value is the correct one, as there should be no duplciates
+
+  def return_highest (self, state): 
+    return np.argmax(self.QM.summary()[self.states.index(state)] )
+  
